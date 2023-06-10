f73292da2927d801ddce120c78a20e9362ddc6e1
Pajri Zahrawaani Ahmad <pajriblueholigand@gmail.com>
2023-06-10 20:21:15 +0700

    test post

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
+import { AwsClient, S3 } from 'aws4fetch'
+const aws = new AwsClient({
+    accessKeyId : ACCESS_KEY_ID,
+    secretAccessKey : SECRET_ACCESS_KEY,
+    region : REGION_NAME
+})
+const endpoint = ENDPOINT_URL
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
-
-var [rval, rstatus] = ["",{status: 200}]
+var [rval, rstatus] = ["default",{status: 200}]
 class _S3 {
     constructor(pathname,request){
-        let s3path="/s3"
-        switch(pathname){
-            case s3path:
-            case s3path+"/":
-                rval = JSON.stringify({'status':BUCKET_NAME+"KORONGG"})
-                rstatus= {status: 200}
-                break
-            case s3path+"/upload":
-                if(request.method != "POST"){
-                    return new _status({'status': request.method}, 405)
-                }
-                const data = request.text()
-                rval = JSON.stringify({'status':data})
-                break
-            default:
-                rval = JSON.stringify({'status':'korong'})
-                break
-        } 
-        return new Response(rval,rstatus)
+        
     }
 }
 
@@ -47,7 +36,25 @@ async function handleRequest(request) {
     }
     else{
         if(pathname.match(/\/s3/i)){
-            return new _S3(pathname,request)
+            let s3path="/s3"
+            switch(pathname){
+                case s3path:
+                case s3path+"/":
+                    rval = JSON.stringify({'status':BUCKET_NAME+"KORONGG"})
+                    rstatus= {status: 200}
+                    break
+                case s3path+"/upload":
+                    if(request.method != "POST"){
+                        return new _status({'status': request.method}, 405)
+                    }
+                    let data = await request.text()
+                    rval= JSON.stringify({'data' : data},{status: 200})
+                    break
+                default:
+                    rval = JSON.stringify({'status':'korong'})
+                    break
+            } 
+            return new Response(rval,rstatus)
         }
         else{
             return new Response(pathname+" is... "+Boolean(pathname.match(/\/s3/i)))
